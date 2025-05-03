d3ddc92340f8c106ada6644c8b8f5e6292c26c98
yagayyavig <yagayyavig@gmail.com>
2025-05-02 21:56:47 -0700

    new change

--- models/expense.py
+++ models/expense.py
@@ -1,14 +1,15 @@
 from db import db
 from sqlalchemy import DateTime
-from models.category import Category  # needed if using class-based relationship
+
 
 class Expense(db.Model):
     __tablename__ = "expenses"
 
     id = db.mapped_column(db.Integer, primary_key=True)
     amount = db.mapped_column(db.Float, nullable=False)
+    category = db.mapped_column(db.String, nullable=False)
     date = db.mapped_column(DateTime, nullable=False)
     note = db.mapped_column(db.String)
 
     category_id = db.mapped_column(db.Integer, db.ForeignKey("categories.id"))
-    category = db.relationship(Category, back_populates="expenses")  # relationship to Category
+    category = db.relationship("Category", back_populates="expenses")
