de0ded4849e95ed370694498e2857fe8477bd4b1
luisgrm <luisgmorin@gmail.com>
2025-05-04 18:12:13 -0700

    Changed app.py and conftest.py to avoid getting our data inside store.db erased everytime we run our test functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,19 @@
 import pytest
-from app import app as flask_app, db
+from app import create_app, db
 
 @pytest.fixture
 def app():
     # Set up the Flask app in testing mode with in-memory SQLite
-    flask_app.config.update({
+    app = create_app({
         "TESTING": True,
         "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
         "SQLALCHEMY_TRACK_MODIFICATIONS": False
     })
 
-    with flask_app.app_context():
+    with app.app_context():
         from models import category, expense # Ensure all models are registered
         db.create_all()
-        yield flask_app
+        yield app
         db.drop_all()
 
 @pytest.fixture
